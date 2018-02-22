659dc3a9b325d5e187c913ae25edb0cb5416aeec
automationgeek <jf.tremblay@gmail.com>
2018-02-22 15:37:29 -0500

    Create testflair.py

---
+++ testflair.py
@@ -0,0 +1,12 @@
+from flair_api import make_client
+
+client = make_client('n3pdxfil6RboAIFsFiSx0sTSyxzR06SWvoWEiEm3', '77Dmrd52VI8fLw21ioh77xFMvqcsMP24Z2z5zyFqL1jUZCTQhgkl8QNQhGpd', 'https://api.flair.co/')
+
+# retrieve a list of structures available to this account
+structures = client.get('structures')
+
+# get a single room by id
+room = client.get('rooms', id="1")
+
+# fetch vents in a room
+vents = room.get_rel('vents')
