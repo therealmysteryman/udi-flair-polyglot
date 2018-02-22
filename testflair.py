0a2ddcf3d2090c3ccc376de06d94781049c45f99
automationgeek <jf.tremblay@gmail.com>
2018-02-22 18:39:02 -0500

    Update testflair.py

--- testflair.py
+++ testflair.py
@@ -6,7 +6,7 @@ client = make_client('n3pdxfil6RboAIFsFiSx0sTSyxzR06SWvoWEiEm3', '77Dmrd52VI8fLw
 structures = client.get('structures')
 
 # get a single room by id
-room = client.get('rooms', id="1")
+room = client.get('rooms', id="2")
 
 # fetch vents in a room
 # vents = structures.get_rel('vents')
