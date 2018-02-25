19a60014fa092fb3ff427319bd16b8961e286259
automationgeek <jf.tremblay@gmail.com>
2018-02-25 15:10:24 -0500

    Update flair_poly.py

--- flair_poly.py
+++ flair_poly.py
@@ -76,21 +76,22 @@ class Controller(polyinterface.Controller):
             roomNumber = 1
             for room in rooms:
                 strHashRoom = str(int(hashlib.md5(room.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 7))
-                try:
-                    vents = room.get_rel('vents')
-                    for vent in vents :
-                        strHashVents = str(int(hashlib.md5(vent.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
-                        self.addNode(FlairStructure(self, strHash, strHashVents ,'R' + str(roomNumber) + '_' + vent.attributes['name'],vent))
-                except Exception as ex:
-                    pass
                 
-                # try:
-                #    pucks = room.get_rel('pucks')
-                #    for puck in pucks :
-                #        strHashPucks = str(int(hashlib.md5(puck.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
-                #        self.addNode(FlairStructure(self, strHash, strHashPucks,'R' + str(roomNumber) + '_' + puck.attributes['name'],puck))
-                #except Exception as ex2:
+                #try:
+                #    vents = room.get_rel('vents')
+                #    for vent in vents :
+                #        strHashVents = str(int(hashlib.md5(vent.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
+                #        self.addNode(FlairStructure(self, strHash, strHashVents ,'R' + str(roomNumber) + '_' + vent.attributes['name'],vent))
+                #except Exception as ex:
                 #    pass
+                
+                try:
+                    pucks = room.get_rel('pucks')
+                    for puck in pucks:
+                        strHashPucks = str(int(hashlib.md5(puck.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
+                        self.addNode(FlairStructure(self, strHash, strHashPucks,'R' + str(roomNumber) + '_' + puck.attributes['name'],puck))
+                except Exception as ex2:
+                    pass
                 
                 roomNumber = roomNumber + 1
     def delete(self):
