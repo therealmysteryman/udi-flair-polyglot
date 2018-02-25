de191c588df87d169c1d100d20ae58501629d932
automationgeek <jf.tremblay@gmail.com>
2018-02-25 18:54:11 -0500

    Update flair_poly.py

--- flair_poly.py
+++ flair_poly.py
@@ -79,17 +79,17 @@ class Controller(polyinterface.Controller):
              
                 try:
                     pucks = room.get_rel('pucks')
-                    for puck in pucks:
-                        strHashPucks = str(int(hashlib.md5(puck.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
-                        self.addNode(FlairStructure(self, strHash,strHashPucks,'R' + str(roomNumber) + '_' + puck.attributes['name'],puck))
+                    #for puck in pucks:
+                    #    strHashPucks = str(int(hashlib.md5(puck.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
+                    #    self.addNode(FlairStructure(self, strHash,strHashPucks,'R' + str(roomNumber) + '_' + puck.attributes['name'],puck))
                 except Exception as ex:
                     pass
             
                 try:
                     vents = room.get_rel('vents')
-                    for vent in vents :
-                        strHashVents = str(int(hashlib.md5(vent.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
-                        self.addNode(FlairStructure(self, strHash, strHashVents ,'R' + str(roomNumber) + '_' + vent.attributes['name'],vent))
+                    #for vent in vents :
+                    #    strHashVents = str(int(hashlib.md5(vent.attributes['name'].encode('utf8')).hexdigest(), 16) % (10 ** 8))
+                    #    self.addNode(FlairStructure(self, strHash, strHashVents ,'R' + str(roomNumber) + '_' + vent.attributes['name'],vent))
                 except Exception as ex:
                     pass
                 
