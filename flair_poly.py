9de5b8729a80305d9b8b81e5fc33c1d9fdd92f93
automationgeek <jf.tremblay@gmail.com>
2018-02-26 10:36:06 -0500

    Update flair_poly.py

--- flair_poly.py
+++ flair_poly.py
@@ -136,7 +136,7 @@ class FlairVent(polyinterface.Node):
     def start(self):
         self.setDriver('ST', 1)
         current_state = self.objVent.get_rel('current-vent-state')
-        #LOGGER.info(str(current_state.attributes['percent-open']))
+       LOGGER.info(str(current_state.attributes['percent-open']))
         #LOGGER.info(str(current_state.attributes['battery-level']))
         
     def query(self):
